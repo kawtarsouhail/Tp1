097d69a3fc926a9bf667b60274c30bcf298b701e
kawtarsouhail <kawtarsouhail698@gmail.com>
2024-02-21 18:59:03 +0100

    creation de class etudiat

---
+++ etudiant.java
@@ -0,0 +1,12 @@
+public class Etudiant {
+
+    private String nom;
+    private String prenom;
+    private int age;
+
+    public Etudiant(String nom, String prenom, int age) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.age = age;
+    }
+}

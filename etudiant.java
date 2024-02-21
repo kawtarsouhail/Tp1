05002e7098b515162af8244266c0a4990466d0a3
kawtarsouhail <kawtarsouhail698@gmail.com>
2024-02-21 19:05:30 +0100

    modification a la class etudiant avec branch develop

--- etudiant.java
+++ etudiant.java
@@ -10,3 +10,41 @@ public class Etudiant {
         this.age = age;
     }
 }
+ public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Etudiant{" +
+                "nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+
+    public static void main(String[] args) {
+        Etudiant etudiant = new Etudiant("Doe", "John", 20);
+        System.out.println(etudiant);
+    }

00183580fa7b61db9a9ebbf1f8560fc180e5a2b7
kawtarsouhail <kawtarsouhail698@gmail.com>
2024-02-21 19:14:11 +0100

    ajout de attribut note avec main

--- etudiant.java
+++ etudiant.java
@@ -3,6 +3,7 @@ public class Etudiant {
     private String nom;
     private String prenom;
     private int age;
+    private String notes;
 
     public Etudiant(String nom, String prenom, int age) {
         this.nom = nom;
